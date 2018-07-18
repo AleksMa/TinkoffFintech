e792a44fa299d76329ad7ff75fb5287c59ddc160
Alexey Mamaev <aleksma99@yandex.ru>
2018-07-18 14:35:21 +0300

    Update task3.sh

--- Hometasks/HW-3 (Shell)/task3.sh
+++ Hometasks/HW-3 (Shell)/task3.sh
@@ -2,5 +2,5 @@
 if [ -z "$(ps -A | grep httpd)" ]
 then
         ~/homework/materials/class03/src/tinyhttpd/tinyhttpd/httpd
-        echo "$(date  +%Y-%m-%d\ %H:%M:%S) httpd was not running, starting..." $
+        echo "$(date  +%Y-%m-%d\ %H:%M:%S) httpd was not running, starting..." >> ~/httpd_start.log
 fi
